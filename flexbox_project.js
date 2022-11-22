d731e5e33da3dc07273e4141de4e1aeea98169cd
Mani-Manohar <72624026+Mani-Manohar@users.noreply.github.com>
2022-11-22 11:49:08 +0530

    Added code of auto-close sidebar
    If user clicks anywhere out side the sidebar when it is opened the sidebar will automatically close.

--- flexbox_project.js
+++ flexbox_project.js
@@ -8,3 +8,13 @@ hamBgr.addEventListener("click", function(){
     hamBgr.classList.toggle("icon");
     nav.classList.toggle("show");
 });
+
+document.addEventListener("click", function (e) {
+    // console.log(e.target.classList[1]);
+
+    if(e.target.classList[1] !== "icon") {
+        nav.classList.toggle("show");
+        hamBgr.classList.toggle("icon");
+        // console.log("YES");
+    }
+});
